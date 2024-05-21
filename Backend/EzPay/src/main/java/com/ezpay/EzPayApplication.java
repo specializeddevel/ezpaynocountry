7e46f9675b0fad219b2cce833f3dcefdf7ed426d
JAPMagdaleno <121467694+JAPMagdaleno@users.noreply.github.com>
2024-05-21 09:54:24 -0600

    Agrego proyecto estandarizado con DB establecida del Back y una carpeta vacia para Front

---
+++ Backend/EzPay/src/main/java/com/ezpay/EzPayApplication.java
@@ -0,0 +1,13 @@
+package com.ezpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EzPayApplication.class, args);
+    }
+
+}
---
+++ Backend/EzPay/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+TRUNCATE TABLE USERS;
+INSERT INTO USERS (user_id, username, password, email)
+VALUES (3, 'Juan Perez', 'password123', 'juan.perez@example.com'),
+       (4, 'Sebastian Garmendia', 'password456', 'sebastian.garmendia@example.com'),
+       (5, 'Carla Camacho', 'password789', 'carla.camacho@example.com'),
+       (6, 'Cosme Fulanito', 'password101', 'cosme.fulanito@example.com');
+
