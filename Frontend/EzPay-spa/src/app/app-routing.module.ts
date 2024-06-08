3d9ae2d8b7cc973aac266e6f24da6042e52b574b
Pablo2311 <gutierrezpabloraul@gmail.com>
2024-06-07 22:42:14 -0300

    [ADD] register interface

--- Frontend/EzPay-spa/src/app/app-routing.module.ts
+++ Frontend/EzPay-spa/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-// import { HomeComponent } from './layout/home/home.component';
+import { HomeComponent } from './layout/home/home.component';
 import { RegisterComponent } from './auth/register/register.component';
 
 const routes: Routes = [
