6f06a0b52e2d49ee2bb59537fb46966b34574447
DevJohanAdrian <jamaringarciabusiness@gmail.com>
2024-06-07 19:01:07 -0500

    Dashboard

--- Frontend/EzPay-spa/src/app/app-routing.module.ts
+++ Frontend/EzPay-spa/src/app/app-routing.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-// import { HomeComponent } from './layout/home/home.component';
+import { HomeComponent } from './layout/home/home.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/home',
-    pathMatch: 'full'
+    redirectTo: '/dashboard',
+    pathMatch: 'full',
   },
-  { path: 'home', component: HomeComponent},
-  
-  // { path: 'home', loadComponent:()=> import('./layout/home/home.component') },
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    children: [
+      {
+        path: 'inicio',
+        loadComponent: () =>
+          import('./modules/dashboard/pages/inicio/inicio.component'),
+      },
+      // {path:'inicio', loadComponent:()=> import()},
+    ],
+  },
   { path: 'register', component: RegisterComponent },
+
   // Fallback when no prior routes is matched or 404
   { path: '**', redirectTo: '/home', pathMatch: 'full' },
 ];
--- Frontend/EzPay-spa/src/app/app.module.ts
+++ Frontend/EzPay-spa/src/app/app.module.ts
@@ -6,9 +6,12 @@ import { AppComponent } from './app.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { CoreModule } from './core/core.module';
 import {HomeComponent}  from './layout/home/home.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import InicioComponent from './modules/dashboard/pages/inicio/inicio.component';
+
 
 @NgModule({
-  declarations: [AppComponent, RegisterComponent, HomeComponent],
+  declarations: [AppComponent, RegisterComponent, HomeComponent, DashboardComponent, InicioComponent],
   imports: [
     BrowserModule, 
     //features
